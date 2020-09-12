c85abc456aa806c632baa21dbd01859d34d285c7
Oleg Gavrilich <gavrilich.oleg.lviv@gmail.com>
2020-09-12 16:48:04 -0700

    Project created

---
+++ ListingsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListingsApp
+//
+//  Created by Oleg Gavrilich on 12.09.2020.
+//  Copyright © 2020 Oleg Gavrilich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
